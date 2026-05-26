00464c006d7ec7b11c3a3250c9791f4118f6b5b3
ito-emtech <00002301027@st.emtech-academy.com>
2026-05-26 13:04:42 +0900

    プロジェクト ファイルを追加します。

---
+++ FIT_Technology/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FIT_Technology.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
