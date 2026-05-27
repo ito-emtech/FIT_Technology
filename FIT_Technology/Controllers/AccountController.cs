5c45aeba52ec52dad231e186d626cafd6adac0b1
ito-emtech <00002301027@st.emtech-academy.com>
2026-05-27 17:35:03 +0900

    テスト

--- FIT_Technology/Controllers/AccountController.cs
+++ FIT_Technology/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DynamicDll.Db;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using FIT_Technology.Models.Daos;
 
 namespace FIT_Technology.Controllers
 {
@@ -10,10 +13,40 @@ namespace FIT_Technology.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Index(string name)
+        {
+            return View();
+        }
+
         [HttpGet]
         public IActionResult Login()
         {
             return View(nameof(AccountController.Login));
+        }
+
+        [HttpPost]
+        public IActionResult Login(string userid, string password)
+        {
+            throw new NotImplementedException();
+
+            using (TranMng tm = TranMng.BeginTransaction("empdb"))
+            {
+                AccountDao accountDao = new AccountDao();
+
+                var userName = AccountDao.Login(userid, password);
+
+                if (string.IsNullOrEmpty(userName))
+                {
+                    return RedirectToAction(nameof(Error));
+                }
+                else
+                {
+                    HttpContext.Session.SetString(nameof(userName), userName);
+                    HttpContext.Session.SetString(nameof(userid), userid);
+                    return RedirectToAction(nameof(HomeController.Menu));
+                }
+            }
         }
 
         [HttpGet]
--- FIT_Technology/Models/Daos/AccountDao.cs
+++ FIT_Technology/Models/Daos/AccountDao.cs
@@ -1,5 +1,7 @@
 ﻿using DynamicDll.Db;
 using FIT_Technology.Models.Entities;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace FIT_Technology.Models.Daos
 {
@@ -9,7 +11,38 @@ namespace FIT_Technology.Models.Daos
 
         public override UserEntity Find(params object[] pkeys)
         {
-            throw new NotImplementedException();
+            // 引数チェック: 主キーが渡されていない場合は null を返す
+            if (pkeys == null || pkeys.Length == 0 || pkeys[0] == null)
+            {
+                return new UserEntity();
+            }
+
+            string query = @"
+                SELECT user_id, password
+                FROM m_user 
+                WHERE user_id = @UserId";
+
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                cmd.Transaction = trn;
+
+                // パラメータ設定
+                cmd.Parameters.Add("@UserId", SqlDbType.VarChar).Value = pkeys[0];
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        UserEntity user = new UserEntity();
+                        user.UserId = reader["user_id"].ToString().Trim();
+                        user.Password = reader["password"].ToString().Trim();
+
+                        reader.Close();
+                        return user;
+                    }
+                }
+                return new UserEntity();
+            }
         }
 
         public override int Insert(UserEntity entity)
@@ -25,6 +58,23 @@ namespace FIT_Technology.Models.Daos
         public override int Delete(UserEntity entity)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// ログインチェックを行います
+        /// </summary>
+        public string? Login(string userid, string password)
+        {
+            if (string.IsNullOrEmpty(userid) || string.IsNullOrEmpty(password))
+                return null;
+
+            UserEntity user = Find(userid);
+
+            if (user == null) return null;
+
+            if (user.Password != password) return null;
+
+            return user.UserId;
         }
     }
 }
