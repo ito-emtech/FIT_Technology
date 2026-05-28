aee79041c4bcef5025f9f3c5aeb48f1e38b1e1ce
f <user@PC-NUMBER>
2026-05-28 09:39:26 +0900

    Employeeを更新しました

--- FIT_Technology/Controllers/EmployeeController.cs
+++ FIT_Technology/Controllers/EmployeeController.cs
@@ -4,9 +4,34 @@ namespace FIT_Technology.Controllers
 {
     public class EmployeeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View(nameof(EmployeeController.Index));
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return View(nameof(EmployeeController.List));
+        }
+
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            return View(nameof(EmployeeController.Insert));
+        }
+
+        [HttpGet]
+        public IActionResult Alert()
+        {
+            return View(nameof(EmployeeController.Alert));
+        }
+
+        [HttpGet]
+        public IActionResult Update()
+        {
+            return View(nameof(EmployeeController.Update));
         }
     }
 }
