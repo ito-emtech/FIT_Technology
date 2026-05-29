24858d63dc8a85faf004e23353668cbee2dc1c67
ito-emtech <00002301027@st.emtech-academy.com>
2026-05-29 13:18:14 +0900

    LiceseDaoのひな形を作成

---
+++ FIT_Technology/Models/Daos/LicenseDao.cs
@@ -0,0 +1,31 @@
+﻿using DynamicDll.Db;
+using FIT_Technology.Models.Entities;
+
+namespace FIT_Technology.Models.Daos
+{
+    public class LicenseDao : BaseDao<EmployeeEntity>
+    {
+        public LicenseDao() { }
+
+        public override EmployeeEntity Find(params object[] pkeys)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Insert(EmployeeEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Update(EmployeeEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Delete(EmployeeEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
